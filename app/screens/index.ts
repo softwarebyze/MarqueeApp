e2dfc889c21367744f3228d57093ff5cf082302b
Zachary Ebenfeld <zackebenfeld@gmail.com>
2024-01-02 13:50:38 -0500

    removed LoginScreen export

--- app/screens/index.ts
+++ app/screens/index.ts
@@ -1,6 +1,5 @@
 export * from "./WelcomeScreen"
 
-export * from "./LoginScreen"
 export * from "./DemoCommunityScreen"
 export * from "./DemoDebugScreen"
 export * from "./DemoShowroomScreen/DemoShowroomScreen"
