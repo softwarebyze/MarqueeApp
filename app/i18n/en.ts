4a539ee9b32dab5e7adb9a54ed2d1625790146bb
Zachary Ebenfeld <zackebenfeld@gmail.com>
2024-01-02 13:52:07 -0500

    removed LoginScreen translations

--- app/i18n/en.ts
+++ app/i18n/en.ts
@@ -30,17 +30,6 @@ const en = {
   errors: {
     invalidEmail: "Invalid email address.",
   },
-  loginScreen: {
-    signIn: "Sign In",
-    enterDetails:
-      "Enter your details below to unlock top secret info. You'll never guess what we've got waiting. Or maybe you will; it's not rocket science here.",
-    emailFieldLabel: "Email",
-    passwordFieldLabel: "Password",
-    emailFieldPlaceholder: "Enter your email address",
-    passwordFieldPlaceholder: "Super secret password here",
-    tapToSignIn: "Tap to sign in!",
-    hint: "Hint: you can use any email address and your favorite password :)",
-  },
   demoNavigator: {
     componentsTab: "Components",
     debugTab: "Debug",
