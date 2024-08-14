ef5d32327a399d9b8aceccbf7393a858bddca73c
Zachary Ebenfeld <zackebenfeld@gmail.com>
2024-08-14 14:38:28 -0400

    delete everything

--- app/screens/index.ts
+++
@@ -1,9 +0,0 @@
-export * from "./HomeScreen"
-
-export * from "./DemoCommunityScreen"
-export * from "./DemoDebugScreen"
-export * from "./DemoShowroomScreen/DemoShowroomScreen"
-
-export * from "./ErrorScreen/ErrorBoundary"
-// export other screens here
-export * from "./ViewScreen"
