7e77d61e303cd0591e4ef056a8c405c500565205
Zachary Ebenfeld <zackebenfeld@gmail.com>
2024-10-14 14:47:27 -0400

    sharing only the url

--- app/utils/shareMarquee.ts
+++ app/utils/shareMarquee.ts
@@ -6,8 +6,6 @@ export async function shareMarquee(text: string) {
     const encodedText = encode(text)
     const shareUrl = `https://marqueeapp.netlify.app/view/${encodedText}`
     const result = await Share.share({
-      title: "Share Marquee",
-      message: `Check out my marquee:`,
       url: shareUrl, // This is used by iOS
     })
     if (result.action === Share.sharedAction) {
